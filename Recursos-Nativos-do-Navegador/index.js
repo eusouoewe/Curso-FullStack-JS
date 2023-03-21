25a0acedca466009cd2bae46db508a035ef6877c
Ewerton Fernandes dos Santos <92743537+eusouoewe@users.noreply.github.com>
2023-03-21 17:09:04 -0300

    Aprendendo sobre recursos uteis que já vem automaticamente no navegador

---
+++ Recursos-Nativos-do-Navegador/index.js
@@ -0,0 +1,8 @@
+let nome = "Ewerton"
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+//window.alert(nome)
+//nome = window.prompt("Insira seu nome: ")
+//let confirmacao = window.confirm("Confirma esta ação?")
