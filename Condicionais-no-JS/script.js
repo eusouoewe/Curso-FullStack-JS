20f09ab5754ccacce0363b1f7b3db37e8e2f9496
Ewerton Fernandes dos Santos <92743537+eusouoewe@users.noreply.github.com>
2023-03-22 17:26:47 -0300

    Aprendendo sobre condicionais e operador ternário

---
+++ Condicionais-no-JS/script.js
@@ -0,0 +1,15 @@
+// const idade = prompt("Insira aqui sua idade: ");
+
+// if (idade > 18) {
+//     alert("Você é maior de idade");
+// } else if (idade > 12) {
+//     alert("Você é menor de idade");
+// } else if (idade > 4) {
+//     alert("Você é uma criança")
+// } else {
+//     alert("...")
+// }
+
+const resultado = (6 === 5) ? "verdadeiro" : "falso"; //mostrará falso
+
+alert(resultado)
