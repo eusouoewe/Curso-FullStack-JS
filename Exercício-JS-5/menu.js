7e4053eb9d68e32d16d8236d4b61777b8650770e
Ewerton Fernandes dos Santos <92743537+eusouoewe@users.noreply.github.com>
2023-03-23 19:30:17 -0300

    fazendo melhorias no script

--- Exercício-JS-5/menu.js
+++ Exercício-JS-5/menu.js
@@ -7,7 +7,26 @@ do {
     "\n3-  Opção 3;" +
     "\n4-  Opção 4;" +
     "\n5-  Encerrar.")
-    alert(`A opção escolhida foi ${opcao}.`);
+
+    switch (opcao) {
+        case 1:
+            alert(`A opção escolhida foi ${opcao}.`);
+            break
+        case 2: 
+        alert(`A opção escolhida foi ${opcao}.`);
+        break
+        case 3:
+            alert(`A opção escolhida foi ${opcao}.`);
+            break
+        case 4:
+            alert(`A opção escolhida foi ${opcao}.`);
+            break
+        case 5:
+            alert("Você escolheu encerrar o processo.");
+            break
+        default:
+            alert("Opção Inválida, escolha apenas uma das opções disponíveis.");
+    }
 } while (opcao !== 5);
 
 alert("O Menu Interativo está sendo encerrado...")
