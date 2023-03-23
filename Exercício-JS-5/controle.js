a4de4d66a014e0aa00ae91f89c22df59b4cf26eb
Ewerton Fernandes dos Santos <92743537+eusouoewe@users.noreply.github.com>
2023-03-23 19:19:21 -0300

    Exercício de controle financeiro, que permite depositar ou sacar valores e mostra o valor final.

---
+++ Exercício-JS-5/controle.js
@@ -0,0 +1,21 @@
+const nome = prompt("Olá! Qual o seu nome? ")
+let quantidade = +prompt("Quanto dinheiro você tem? ")
+let novaQuantidade = quantidade;
+let opcao;
+
+do {
+    opcao = +prompt("Você quer: " +
+    "\n1- Depositar quantia;" +
+    "\n2- Sacar quantia." +
+    "\n3- Sair")
+    if (opcao === 1) {
+        depositar = +prompt("Quanto dinheiro você quer depositar? ")
+        novaQuantidade += depositar;
+    } else if (opcao === 2) {
+        sacar = +prompt("Quanto dinheiro você quer sacar? ")
+        novaQuantidade -= sacar;
+    }
+    alert(`Você agora tem R$${novaQuantidade}.00`)
+} while (opcao !== 3)
+
+alert(`Você encerrou o Atendimento Financeiro com R$${novaQuantidade}.00 na conta.`)
