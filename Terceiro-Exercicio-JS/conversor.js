5407a7e37d405bb34d8a8535efbffa5be60d8d6b
Ewerton Fernandes dos Santos <92743537+eusouoewe@users.noreply.github.com>
2023-03-23 16:57:49 -0300

    Adicionando descrição aos case

--- Terceiro-Exercicio-JS/conversor.js
+++ Terceiro-Exercicio-JS/conversor.js
@@ -17,27 +17,27 @@ if (isNaN(medida)) {
     switch (valor) {
         case "mm":
             novaMedida = medida * 1000;
-            alert(`A medida de ${medida} metros em Milímetros se tornou ${novaMedida}`);
+            alert(`A medida de ${medida} metros em Milímetros se tornou ${novaMedida}mm`);
             break;
         case "cm":
             novaMedida = medida * 100;
-            alert(`A medida de ${medida} metros em Centímetros se tornou ${novaMedida}`);
+            alert(`A medida de ${medida} metros em Centímetros se tornou ${novaMedida}cm`);
             break;
         case "dm":
             novaMedida = medida * 10;
-            alert(`A medida de ${medida} metros em Decímetros se tornou ${novaMedida}`);
+            alert(`A medida de ${medida} metros em Decímetros se tornou ${novaMedida}dm`);
             break;
         case "dam":
             novaMedida = medida / 10;
-            alert(`A medida de ${medida} metros em Decâmetros se tornou ${novaMedida}`);
+            alert(`A medida de ${medida} metros em Decâmetros se tornou ${novaMedida}dam`);
             break;
         case "hm":
             novaMedida = medida / 100;
-            alert (`A medida de ${medida} metros em Hectômetros se tornou ${novaMedida}`);
+            alert (`A medida de ${medida} metros em Hectômetros se tornou ${novaMedida}hm`);
             break;
         case "km":
             novaMedida = medida / 1000;
-            alert(`A medida de ${medida} metros em Quilômetros se tornou ${novaMedida}`);
+            alert(`A medida de ${medida} metros em Quilômetros se tornou ${novaMedida}km`);
             break;
         default:
             alert("Opção Inválida! Escoha uma das opções.");
