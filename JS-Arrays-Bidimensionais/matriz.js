a99c3e1f42469901bb7d66d1d5a2548583cf9f89
Ewerton Fernandes dos Santos <92743537+eusouoewe@users.noreply.github.com>
2023-03-29 21:12:41 -0300

    Aprendendo sobre Arrays Bidimensionais e como interagir com elas

---
+++ JS-Arrays-Bidimensionais/matriz.js
@@ -0,0 +1,32 @@
+const arr = [
+    "1º Nível",
+    ["2º Nível", "jose", 15, true],
+    [
+        ["3º Nível, 1º item", "Olá, mundo!"],
+        ["3º Nível, 2º item", "Olá, mundo!"],
+    ],
+[]
+]
+
+console.log(arr)
+console.log(arr[0])
+console.log(arr[1][0])
+console.log(arr[1][3])
+console.log(arr[2][1][0])
+
+const matriz = [
+    ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
+    ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
+    ["l3, c1", "l3, c2", "l3, c3", "l3, c4"],
+]
+
+// matriz.push(["nova linha"])
+// matriz[0].push("nova coluna")
+// console.table(matriz)
+
+for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+        const elemento = matriz[i][j];
+        console.log(`Posição: (${i}, ${j}, valor: ${elemento}`)
+    }
+}
