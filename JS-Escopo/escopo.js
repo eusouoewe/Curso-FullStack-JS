1e3e0851d55d6f3d08e379ae6659e7b4925facbe
Ewerton Fernandes dos Santos <92743537+eusouoewe@users.noreply.github.com>
2023-03-30 11:29:45 -0300

    Aprendendo sobre escopo

---
+++ JS-Escopo/escopo.js
@@ -0,0 +1,48 @@
+let pokemon = "Charmander";
+
+function evoluir() {
+    pokemon = "Charmeleon";
+}
+
+console.log(pokemon);
+evoluir();
+console.log(pokemon);
+
+// function criarAnimal() {
+//     let animal = "Gato"
+// }
+
+// criarAnimal();
+// console.log(animal);
+
+// function avaliarNota(nota) {
+//     if (nota > 60) {
+//         var aprovado = true
+//         let situacao = "Aprovado"
+//     } else {
+//         var aprovado = false
+//         let situacao = "Reprovado"
+//     }
+// }
+
+// console.log(nota)
+// console.log(aprovado)
+// console.log(situacao)
+
+// avaliarNota(72)
+// avaliarNota(41)
+
+function ola() {
+    var texto = "olá, mundo"
+}
+
+// console.log(texto)
+
+console.log(nome)
+// console.log(sobrenome)
+
+var nome = ("Ewerton")
+let sobrenome = ("Fernandes")
+
+console.log(nome)
+// console.log(sobrenome)
